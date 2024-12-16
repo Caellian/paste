981e714dfadf14f077c68993db8a527dce269c21
Tin Švagelj <tin.svagelj@live.com>
2024-12-17 00:28:26 +0100

    Add test for lowerCamel
    Signed-off-by: Tin Švagelj <tin.svagelj@live.com>

--- tests/test_item.rs
+++ tests/test_item.rs
@@ -163,6 +163,29 @@ mod test_to_camel {
     }
 }
 
+mod test_to_lowercamel {
+    use paste::paste;
+
+    macro_rules! m {
+        ($id:ident) => {
+            paste! {
+                const DEFAULT_CAMEL: &str = stringify!([<$id:lowerCamel>]);
+                const LOWER_CAMEL: &str = stringify!([<$id:lowerCamel:lower>]);
+                const UPPER_CAMEL: &str = stringify!([<$id:lowerCamel:upper>]);
+            }
+        };
+    }
+
+    m!(this_is_but_a_test);
+
+    #[test]
+    fn test_to_camel() {
+        assert_eq!(DEFAULT_CAMEL, "thisIsButATest");
+        assert_eq!(LOWER_CAMEL, "thisisbutatest");
+        assert_eq!(UPPER_CAMEL, "THISISBUTATEST");
+    }
+}
+
 mod test_doc_expr {
     // https://github.com/dtolnay/paste/issues/29
 
